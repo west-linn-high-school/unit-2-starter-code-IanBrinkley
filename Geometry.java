684795e62c56ab70d1a2682e103651d153e056f6
Michael George <georgem@wlwv.k12.or.us>
2019-09-10 09:35:08 -0700

    this is not a commit i promise

--- Geometry.java
+++ Geometry.java
@@ -2,10 +2,10 @@ public class Geometry {
     public static void main(String[] args) {
         int sides = 7;
         double radius = 5.0;
-        final double PI = 22/7;
+        final double PI = 3.1;
         System.out.println("A heptagon has " + sides + " sides.");
-        System.out.println("A decagon has " + (sides + 3) + " sides.");
-        System.out.println("A dodecagon has " + (sides + 5) + " sides.");
-        System.out.println("The circumference of a circle with a radius of " + radius + " is approximately " + 2*PI*radius + ".");
+        System.out.println("A decagon has " + (sides + (sides+sides+sides)/sides) + " sides.");
+        System.out.println("A dodecagon has " + (sides*((sides+sides)/sides)-((sides+sides)/sides) + " sides."));
+        System.out.println("The circumference of a circle with a radius of " + radius + " is approximately " + (sides+sides)*PI*radius + ".");
     }
 }
--- test.java
+++ test.java
@@ -1,10 +1,14 @@
 public class test {
     public static void main(String[] args) {
-        String m1, m2, m3;
-        m1 = "Quest for the Holy Grail";
-        m2 = m1.toLowerCase();
-        m3 = m1 + " " + m2;
-        System.out.println(m3.replace('h', 'z'));
-
+        /*int a = 40;
+        int b = 6;
+        int c;
+        double x = 40.0;
+        double y = 6.0;
+        double z;
+        z = (double) a/b;
+        System.out.println(z);*/
+        int w = 2, y = 7, z = 12;
+        System.out.println(w*z--+y++);
     }
 }
